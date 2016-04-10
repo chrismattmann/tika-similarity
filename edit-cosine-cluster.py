d1b8d5b3cafed57b21ab8ec7def7cff53a2b972f
Aravind Ram Nathan <AravindRam@users.noreply.github.com>
2016-04-10 12:54:41 -0700

    Added command line options for cluster D3

--- edit-cosine-cluster.py
+++ edit-cosine-cluster.py
@@ -19,46 +19,65 @@
 
 
 import json
-import sys
+import argparse
 import csv
 
-row=[]
-csvPath = sys.argv[1]       #Input Path to csv file
-with open(csvPath,"r") as f:
-    lines = csv.reader(f.read().splitlines(), delimiter=' ')
-    for line in lines:
-        row.append(line)
+def createCluster(inputCSV,argNum):
+    row=[]
+    csvPath = inputCSV          #Input Path to csv file
+    with open(csvPath,"r") as f:
+        lines = csv.reader(f.read().splitlines(), delimiter=' ')
+        for line in lines:
+            row.append(line)
 
-data={}
-for i in range(len(row)):
-    if "x-coordinate" in row[i][0].split(","):
-        continue
-    else:
-        column = row[i][0].split(",")
-        data[column[0]]=[]
+    data={}
+    for i in range(len(row)):
+        if "x-coordinate" in row[i][0].split(","):
+            continue
+        else:
+            column = row[i][0].split(",")
+            if argNum == 0:                 #Cluster based on x-coordinate
+                data[column[0]]=[]
+            elif argNum == 1:               #Cluster based on y-coordinate
+                data[column[1]]=[]
+            elif argNum == 2:               #Cluster based on similarity score
+                data[column[2]]=[]
 
-for i in range(len(row)):
-    if "x-coordinate" in row[i][0].split(","):
-        continue
-    else:
-        column = row[i][0].split(",")
-        second={}
-        second["name"]=column[1]+"  "+column[2]
-        second["size"]=column[2]
-        data[column[0]].append(second)
+    for i in range(len(row)):
+        if "x-coordinate" in row[i][0].split(","):
+            continue
+        else:
+            column = row[i][0].split(",")
+            second={}
+            second["name"]=column[1]+"  "+column[2]
+            second["size"]=column[2]
+            if argNum == 0:                 #Cluster based on x-coordinate
+                data[column[0]].append(second)
+            elif argNum == 1:               #Cluster based on y-coordinate
+                data[column[1]].append(second)
+            elif argNum == 2:               #Cluster based on similarity score
+                data[column[2]].append(second)
 
-clusterList = []
-i=0
-for elem in data.keys():
-    first={}
-    first["name"]="cluster "+str(i)
-    first["children"]=data[elem]
-    clusterList.append(first)
-    i+=1
+    clusterList = []
+    i=0
+    for elem in data.keys():
+        first={}
+        first["name"]="cluster "+str(i)
+        first["children"]=data[elem]
+        clusterList.append(first)
+        i+=1
 
 
-print json.dumps(clusterList, sort_keys=True, indent=4, separators=(',', ': '))
+    print json.dumps(clusterList, sort_keys=True, indent=4, separators=(',', ': '))
 
-clusterStruct = {"name":"clusters", "children":clusterList}
-with open("clusters.json", "w") as f:             #Pass the json file as input to cluster-d3.html or dynamic-cluster.html
-    f.write(json.dumps(clusterStruct, sort_keys=True, indent=4, separators=(',', ': ')))
+    clusterStruct = {"name":"clusters", "children":clusterList}
+    with open("clusters.json", "w") as f:             #Pass the json file as input to cluster-d3.html or dynamic-cluster.html
+        f.write(json.dumps(clusterStruct, sort_keys=True, indent=4, separators=(',', ': ')))
+
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser('Edit/Cosine Similarity Cluster-D3')
+    argParser.add_argument('--inputCSV', required=True, help='path to directory containing CSV File, containing pair-wise similarity scores based on edit/cosine similarity distance')
+    argParser.add_argument('--cluster', required=True, type=int, help='Cluster based on the x-coordinate/y-coordinate/similarity score. Pass 0 for clustering based on x-coordinate, 1 for clustering based on y-coordinate, 2 for clustering based on similarity score')
+    args = argParser.parse_args()
+
+    createCluster(args.inputCSV, args.cluster)
