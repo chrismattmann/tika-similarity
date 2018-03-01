92f13ebf645bccc28a4eee6a4f9630dc26a5939d
Aditya <adi@AdisMacAir.local>
2018-03-01 13:16:12 -0800

    added func in edit and cos similarity for computing sim score for each row combination in a file

--- edit-value-similarity.py
+++ edit-value-similarity.py
@@ -22,6 +22,7 @@ import os, editdistance, itertools, argparse, csv
 from requests import ConnectionError
 from time import sleep
 import json
+import ast
 
 
 def stringify(attribute_value):
@@ -170,16 +171,66 @@ def compute_score2(json_input_list, outCSV, acceptTypes, allKeys):
     return
 
 
+def computeScores3(inputFile, outCSV):
+    na_metadata = ["resourceName"]
+    with open(outCSV, "wb") as outF:
+        a = csv.writer(outF, delimiter=',')
+        a.writerow(["x-coordinate", "y-coordinate", "Similarity_score"])
+
+        file1_parsedData = parser.from_file(inputFile)
+        row_list = ast.literal_eval(file1_parsedData["content"])
+
+        rows_tuple = itertools.combinations(row_list, 2)
+        for row1, row2 in rows_tuple:
+            try:
+                row_edit_distance = [row_list.index(row1), row_list.index(row2)]
+
+                content_1 = row1 #ast.literal_eval(file1_parsedData["content"])[0]
+                content_2 = row2 #ast.literal_eval(file2_parsedData["content"])[0]
+
+                intersect_features = set(content_1.keys()) & set(content_2.keys())
+
+                intersect_features = [feature for feature in intersect_features if feature not in na_metadata]
+
+                file_edit_distance = 0.0
+                for feature in intersect_features:
+
+                    file1_feature_value = stringify(content_1[feature])
+                    file2_feature_value = stringify(content_2[feature])
+
+                    if len(file1_feature_value) == 0 and len(file2_feature_value) == 0:
+                        feature_distance = 0.0
+                    else:
+                        feature_distance = float(editdistance.eval(file1_feature_value, file2_feature_value)) / (
+                        len(file1_feature_value) if len(file1_feature_value) > len(file2_feature_value) else len(
+                            file2_feature_value))
+
+                    file_edit_distance += feature_distance
+
+                file_edit_distance /= float(len(intersect_features))  # average edit distance
+
+                row_edit_distance.append(1 - file_edit_distance)
+                a.writerow(row_edit_distance)
+
+            except ConnectionError:
+                sleep(1)
+            except KeyError:
+                continue
+
+
 if __name__ == "__main__":
     
     argParser = argparse.ArgumentParser('Edit Distance Similarity based on Metadata values')
     argParser.add_argument('--inputDir', required=False, help='path to directory containing files')
+    argParser.add_argument('--inputFile', required=False, help='path to input file')
     argParser.add_argument('--outCSV', required=True, help='path to directory for storing the output CSV File, containing pair-wise Similarity Scores based on edit distance')
     argParser.add_argument('--json', nargs='+', required=False, help='several paths to  JSON file containing certain metadata')
     argParser.add_argument('--accept', nargs='+', type=str, help='Optional: compute similarity only on specified IANA MIME Type(s)')
     argParser.add_argument('--allKeys', action='store_true', help='compute edit distance across all keys')
     args = argParser.parse_args()
 
+    if args.inputFile and args.outCSV:
+        computeScores3(args.inputFile, args.outCSV)
     if args.inputDir and args.outCSV:
         computeScores(args.inputDir, args.outCSV, args.accept, args.allKeys)
     if args.json:
