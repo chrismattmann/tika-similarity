83b571d64feb33d216634018d4b60e60a2d849f3
Melissa Tan <32193089+melxtan@users.noreply.github.com>
2025-03-14 07:48:36 -0700

    Update jaccard_similarity.py

--- tikasimilarity/distance/jaccard_similarity.py
+++ tikasimilarity/distance/jaccard_similarity.py
@@ -1,12 +1,30 @@
-import argparse
-import json
-import csv
-import itertools
-import os
-import sys
+#!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+# Computing pairwise jaccard similarity for a given directory of files
+
+from tika import parser
+import os, itertools, argparse, csv
 from functools import reduce
+import sys
+import json
 from tqdm import tqdm
-from tika import parser 
 
 def filterFiles(inputDir, acceptTypes):
     filename_list = []
@@ -17,30 +35,48 @@ def filterFiles(inputDir, acceptTypes):
             if not filename.startswith('.'):
                 filename_list.append(os.path.join(root, filename))
 
-    parsed_results = {filename: parser.from_file(filename) for filename in filename_list}
-
     if acceptTypes:
-        filename_list = [
-            filename for filename in filename_list 
-            if str(parsed_results[filename]['metadata']['Content-Type'].encode('utf-8').decode('utf-8')).split('/')[-1] in acceptTypes
-        ]
+        filtered_files = []
+        for filename in filename_list:
+            metadata = parser.from_file(filename).get("metadata", {})
+            content_type = str(metadata.get("Content-Type", "").encode('utf-8').decode('utf-8')).split('/')[-1]
+            if content_type in acceptTypes:
+                filtered_files.append(filename)
+        filename_list = filtered_files
     else:
         print("Accepting all MIME Types.....")
 
-    return filename_list, parsed_results
+    return filename_list
 
-def compute_jaccard_similarity(obj1, obj2, allKeys=False):
-    keys1 = set(obj1.keys())
-    keys2 = set(obj2.keys())
-
-    if allKeys:
-        union = keys1 | keys2
-        intersection = {key for key in keys1 & keys2 if obj1[key] == obj2[key]}
+def computeScores(inputDir, outCSV, acceptTypes):
+    if sys.version_info[0] >= 3:
+        openMode = "w"
     else:
-        intersection = {key for key in keys1 & keys2 if obj1[key] == obj2[key]}
-        union = keys1 & keys2  
+        openMode = "wb"
+        
+    with open(outCSV, openMode) as outF:
+        a = csv.writer(outF, delimiter=',')
+        a.writerow(["x-coordinate", "y-coordinate", "Similarity_score"])
 
-    return len(intersection) / len(union) if len(union) > 0 else 0.0
+        files_list = filterFiles(inputDir, acceptTypes)
+        
+        parsed_results = {}
+        for filename in tqdm(files_list, desc="Parsing files", unit="file"):
+            parsed_results[filename] = parser.from_file(filename)
+
+        files_tuple = list(itertools.combinations(files_list, 2))
+
+        for file1, file2 in tqdm(files_tuple, desc="Computing file similarities", unit="pair"):
+            f1MetaData = parsed_results[file1]["metadata"]
+            f2MetaData = parsed_results[file2]["metadata"]
+
+            isCoExistant = lambda k: (k in f2MetaData) and (f1MetaData[k] == f2MetaData[k])
+            intersection = reduce(lambda m, k: (m + 1) if isCoExistant(k) else m, list(f1MetaData.keys()), 0)
+
+            union = len(list(f1MetaData.keys())) + len(list(f2MetaData.keys())) - intersection
+            jaccard = float(intersection) / union if union > 0 else 0.0
+
+            a.writerow([file1, file2, jaccard])
 
 def process_json(input_file, output_csv, json_key, allKeys):
     with open(input_file, 'r', encoding='utf-8') as f:
@@ -56,46 +92,42 @@ def process_json(input_file, output_csv, json_key, allKeys):
         writer = csv.writer(f)
         writer.writerow(["x-coordinate", "y-coordinate", "Similarity_score"])
 
-        for obj1, obj2 in tqdm(itertools.combinations(object_list, 2), total=len(object_list) * (len(object_list) - 1) // 2, desc="Computing similarities"):
+        obj_pairs = list(itertools.combinations(object_list, 2))
+        for obj1, obj2 in tqdm(obj_pairs, desc="Computing JSON similarities", unit="pair"):
             similarity_score = compute_jaccard_similarity(obj1, obj2, allKeys)
             writer.writerow([obj1.get('id', 'Unknown'), obj2.get('id', 'Unknown'), similarity_score])
 
-def computeScores(inputDir, outCSV, acceptTypes):
-    if sys.version_info[0] >= 3:
-        openMode = "w"
+def compute_jaccard_similarity(obj1, obj2, allKeys=False):
+    keys1 = set(obj1.keys())
+    keys2 = set(obj2.keys())
+
+    if allKeys:
+        union = keys1 | keys2
+        intersection = {key for key in keys1 & keys2 if obj1[key] == obj2[key]}
     else:
-        openMode = "wb"
-        
-    with open(outCSV, openMode) as outF:
-        a = csv.writer(outF, delimiter=',')
-        a.writerow(["x-coordinate", "y-coordinate", "Similarity_score"])
+        intersection = {key for key in keys1 & keys2 if obj1[key] == obj2[key]}
+        union = keys1 & keys2  
 
-        files_list, parsed_results = filterFiles(inputDir, acceptTypes)
+    return len(intersection) / len(union) if len(union) > 0 else 0.0
 
-        files_tuple = itertools.combinations(files_list, 2)
-
-        for file1, file2 in files_tuple:
-            f1MetaData = parsed_results[file1]["metadata"]
-            f2MetaData = parsed_results[file2]["metadata"]
-
-            isCoExistant = lambda k: (k in f2MetaData) and (f1MetaData[k] == f2MetaData[k])
-            intersection = reduce(lambda m, k: (m + 1) if isCoExistant(k) else m, list(f1MetaData.keys()), 0)
-
-            union = len(list(f1MetaData.keys())) + len(list(f2MetaData.keys())) - intersection
-            jaccard = float(intersection) / union if union > 0 else 0.0
-
-            a.writerow([file1, file2, jaccard])
-
-def main():
-    argParser = argparse.ArgumentParser(description="Compute pairwise similarity for JSON objects.")
-    argParser.add_argument('--inputFile', required=True, help='Path to the input JSON file.')
-    argParser.add_argument('--outCSV', required=True, help='Path to the output CSV file.')
-    argParser.add_argument('--jsonKey', required=True, help='JSON object list key')
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser('Jaccard similarity based file metadata')
+    argParser.add_argument('--inputDir', required=False, help='Path to directory containing files')
+    argParser.add_argument('--inputFile', required=False, help='Path to input JSON file')
+    argParser.add_argument('--outCSV', required=True, help='Path to directory for storing the output CSV file')
+    argParser.add_argument('--jsonKey', required=False, help='JSON object list key (required if using --inputFile).')
     argParser.add_argument('--allKeys', action='store_true', help='Include all keys in Jaccard similarity computation')
+    argParser.add_argument('--accept', nargs='+', type=str, help='Optional: compute similarity only on specified IANA MIME Type(s)')
 
     args = argParser.parse_args()
 
-    process_json(args.inputFile, args.outCSV, args.jsonKey, args.allKeys)
-
-if __name__ == "__main__":
-    main()
+    if args.inputDir:
+        computeScores(args.inputDir, args.outCSV, args.accept)
+    elif args.inputFile:
+        if not args.jsonKey:
+            print("Error: --jsonKey is required when using --inputFile.")
+            sys.exit(1)
+        process_json(args.inputFile, args.outCSV, args.jsonKey, args.allKeys)
+    else:
+        print("Error: Either --inputFile or --inputDir must be provided.")
+        sys.exit(1)
